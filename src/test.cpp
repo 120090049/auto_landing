44e7683a2866da5559b3911fd20313f99fbbb5be
120090049 <120090049@link.cuhk.edu.cn>
2022-08-28 22:35:42 +0800

    master

---
+++ src/test.cpp
@@ -0,0 +1,23 @@
+#include <opencv2/aruco.hpp> 
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <vector>
+
+using namespace cv;
+using namespace std;
+
+
+
+int main(int argc, char** argv)
+{
+    cv::Mat cameraMatrix, distCoeffs;
+
+
+
+
+
+    return 0;
+}
+
+
+
